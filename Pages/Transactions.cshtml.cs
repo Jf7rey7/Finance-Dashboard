52fb509a932047a2b630540d565e98c97032d4c3
Jeffrey Francis <jeffreyfrancis@macbookpro.mynet>
2025-07-04 23:41:27 +0100

    Initial commit - FinanceDashboard working version

---
+++ Pages/Transactions.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinanceDashboard.Pages
+{
+    public class TransactionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        // Other methods like OnPost, etc.
+    }
+}
---
+++ Pages/Upload.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinanceDashboard.Pages
+{
+    public class UploadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        // Other methods like OnPost, etc.
+    }
+}
